02a896b027818a2f22dfc9996720bd6dae0451c0
CyizaLandry5 <52289151+rwema3@users.noreply.github.com>
2026-07-10 02:42:24 +0200

    func df_total

--- analysis.R
+++ analysis.R
@@ -57,3 +57,46 @@ world <- ne_countries(scale = "medium", returnclass = "sf")
 theme_cols <- c("ADP", "CAP", "EQU", "FIN", "GOV", 
                 "IMP", "LND", "MIT", "RSP", "TEC", "TRN")
 
+df_total <- df_filtered %>%
+  mutate(speaker = case_when(
+    speaker == "Columbia" ~ "Colombia",
+    speaker == "Micronesia" ~ "Micronesia (Federated States of)",
+    TRUE ~ speaker
+  )) %>%
+  mutate(iso3 = countrycode(speaker, "country.name", "iso3c")) %>%
+  filter(!is.na(iso3)) %>%
+  mutate(total_mentions = rowSums(across(all_of(theme_cols))))
+
+df_total_allyears = df_total %>%
+  group_by(speaker,iso3) %>%
+  summarise(
+    top_keywords = paste0(top_keywords, collapse = "; "),
+    matched_themes = paste0(matched_themes, collapse = "; "),
+    ADP = sum(ADP), CAP = sum(CAP), EQU = sum(EQU), FIN = sum(FIN),
+    GOV = sum(GOV), IMP = sum(IMP), LND = sum(LND), MIT = sum(MIT),
+    RSP = sum(RSP), TEC = sum(TEC),TRN = sum(TRN),
+    total_mentions = sum(total_mentions)
+  )
+
+world <- ne_countries(scale = "medium", returnclass = "sf")
+map_total <- world %>%
+  left_join(df_total_allyears[, c("speaker", "iso3", "total_mentions")], 
+            by = c("iso_a3" = "iso3"))
+
+P2 <- ggplot(map_total) +
+  geom_sf(aes(fill = total_mentions), color = "grey80") +
+  scale_fill_gradient(
+    low = "#bdd7e7", high = "#08519c", na.value = "grey95",
+    breaks = seq(0, 14, by = 2)
+  ) +
+  labs(title = "", fill = "Total Mentions") +
+  theme_void(base_size = 13) +                         
+  theme(
+    panel.background = element_rect(fill = "white", colour = NA), 
+    plot.background  = element_rect(fill = "white", colour = NA),  
+    legend.position = "right",
+    plot.margin = margin(0, 0, 0, 0)
+  ) +
+  coord_sf(expand = FALSE)
+P2
+##END
